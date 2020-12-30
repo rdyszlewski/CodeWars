62fa82950e7510e9dde1839acc678bb5569bc9af
rdyszlewski <parabitsdev@gmail.com>
2020-12-30 15:31:18 +0100

    Josephus, StripComments

---
+++ src/com/parabbits/strip_comments/StripComments.java
@@ -0,0 +1,36 @@
+package com.parabbits.strip_comments;
+
+import java.util.*;
+
+/*
+Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+Example:
+
+Given an input string of:
+
+apples, pears # and bananas
+grapes
+bananas !apples
+
+The output expected would be:
+
+apples, pears
+grapes
+bananas
+
+The code would be called like so:
+
+var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// result should == "apples, pears\ngrapes\nbananas"
+
+ */
+public class StripComments {
+
+    public static String stripComments(String text, String[] commentSymbols) {
+        String symbols = String.join("", commentSymbols);
+        String regex = String.format("\\s*?[%s][^\\\n]*", symbols);
+        return text.replaceAll(regex,"");
+    }
+
+}
